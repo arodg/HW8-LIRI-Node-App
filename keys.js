a1cba57e159aa58ebbddbd61c2646ee57807e767
Anita Rodgerson <anitarodgerson@Anitas-MacBook-Air.local>
2017-12-15 02:05:08 -0600

    install and save npm packages to json

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+	consumer_key: "ceVGd9N2hxOdgAE5xuNXZHGJp",
+	consumer_secret: "HpRdfSleaT8PIcbHmJu3V4DwaQ2s96wPnDwmlopd0AxWRIknlW",
+	access_token_key: "939043435901628416-2m8hPgKzLnhUYraJ4j3EMA2BiaxkERD",
+	access_token_secret: 	"gz7VrBuKsQ6PYUwOZYMQ4tr4VuhtDqLsiXnvdSA3EAWEf"
+}
+
+module.exports = twitterKeys;
