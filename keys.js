0cc1d92e6d8983e584d7ac9679633d4d399b641d
Anita Rodgerson <anitarodgerson@Anitas-MacBook-Air.local>
2017-12-19 23:02:37 -0600

    Regenerate Twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log("this is loaded");
 
 var twitterKeys = {
-	consumer_key: 'ceVGd9N2hxOdgAE5xuNXZHGJp',
-	consumer_secret: 'HpRdfSleaT8PIcbHmJu3V4DwaQ2s96wPnDwmlopd0AxWRIknlW',
+	consumer_key: 'zecQzTzg2vzDySUuJ6KJlfdSw',
+	consumer_secret: 'jpCDHAYSEniLxJdaPYcFyWdD6qHf3jCilYW24ctLi3dRf5vWYO',
 	access_token_key: '939043435901628416-2m8hPgKzLnhUYraJ4j3EMA2BiaxkERD',
 	access_token_secret: 'gz7VrBuKsQ6PYUwOZYMQ4tr4VuhtDqLsiXnvdSA3EAWEf'
 }
