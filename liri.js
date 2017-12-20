0e79da0fea82f0172ddfdd01c6e0ee1c1862d779
Anita Rodgerson <anitarodgerson@Anitas-MacBook-Air.local>
2017-12-19 23:03:12 -0600

    Default to Ace of Base if no song given

--- liri.js
+++ liri.js
@@ -1,20 +1,24 @@
 
 // 1. Grab data from required files
 
-// A. TWITTER
+// (A) TWITTER
 
 // 2. Store twitter keys in keys.js file so they are private
 // 3. Create function for tweets
-// 4. npm twitter package docs to display tweets
+// 4. npm twitter package to display tweets
 // 5. Display 20 tweets and when they were created
 
 
-// B. SPOTIFIY
+// (B) SPOTIFIY
 
 // 6. Create function for spotify
 // 7. npm node-spotify-api documenation
-// 8.  Display artist, song name, preview, album
+// 8. Display artist, song name, preview, album
 // 9. Default to "The Sign" by Ace of Base
+
+// (C) MOVIE
+
+//
 
 
 // 1. Required files
@@ -24,12 +28,15 @@ var Twitter = require('twitter');
 
 var Spotify = require('node-spotify-api');
 
+var request = require('request');
+
 
 // 3. Create function for tweets
 var myTweets = function () {
 	
 	// 4. Lines 25-44 based on npm documentation for twitter package
 	var client = new Twitter(keys.twitterKeys);
+	
 	
 	// 5. Count is set for 20 tweets
 	var params = {screen_name: 'PixieSput', count: 20};
@@ -51,49 +58,54 @@ var myTweets = function () {
 }
 
 
-
+// 6. Function for Spotify
 var mySpotify = function(title) {
-// 7. Copied from npm node-Spotify-API documentation
+
+// 7. From npm node-Spotify-API documentation
 	var spotify = new Spotify({
 		id: "f837e7ccf2db4ffe80e97bec1003e4a2",
 		secret: "d0974c3068694cdb8692898f055886c8"
 	});
- 
-	spotify.search({type: 'track', query: title}, function(err, data) {
-		if (err) {
-			return console.log('Error occurred: ' + err);
-		}
-		if (title === "The Sign") {
-			spotify.search({type: 'track', query: "The Sign"}, function(err, data) {
-				console.log(data);
-				return;
-			});
-		}
- 		// Per Spotify docs, search type "track" will return track object wrapped in paging object
- 		// "items" in paging object will give array of objects of requested data
- 		// Artist: items.artist.name
- 		// Song: items.name
- 		// Preview: items.preview_url
- 		// Album: items.album.name
- 		var info = data.tracks.items;
- 		for (var i = 0; i < info.length; i+=1) {
- 			console.log("Artist: " + info[i].artists[0].name);
-			console.log("Song: " + info[i].name);
-			console.log("Preview: " + info[i].preview_url);
-			console.log("Album: " + info[i].album.name);
-			console.log("---------------------------------------");
-		}
-	});
-}
 
+	if (title == undefined) {
+		spotify
+  		.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
+  		.then(function(data) {
+ 				console.log("Song: " + data.name);
+ 				console.log("Album: " + data.album.name);
+				console.log("Artist: " + data.album.artists[0].name);
+				console.log("---------------------------------------");
+  		})
+  		.catch(function(err) {
+    		console.error('Error occurred: ' + err); 
+  		});
+  		return;
+	}
+ 
+		spotify
+		.search({ type: 'track', query: title })
+	 	
+	 	.then(function(response) {
+ 			var info = response.tracks.items;
+ 			for (var i = 0; i < info.length; i+=1) {
+ 				console.log("Artist: " + info[i].artists[0].name);
+				console.log("Song: " + info[i].name);
+				console.log("Preview: " + info[i].preview_url);
+				console.log("Album: " + info[i].album.name);
+				console.log("---------------------------------------");
+			}
+		})
+	 
+	 	.catch(function(err) {
+	 		console.log(err);
+	 	});
+}	 	
 
 
 // 6. Switch statement
 var command = process.argv[2];
 var title = process.argv[3];
-if (title == undefined) {
-	title = "The Sign";
-}
+
 	
 switch(command) {
 	case "my-tweets" :
@@ -113,7 +125,7 @@ switch(command) {
 		break;
 
 		default:
-		console.log("Must use specified commands");
+		console.log("Use one of the following commands: my-tweets, spotify-this-song, movie-this, do-what-it-says");
 }
 
 	
